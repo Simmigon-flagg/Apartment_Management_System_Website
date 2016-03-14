a7846ae9ceebd83fb1cca3fda2936f08dd3f4690
Forrestbob <forrestbob2000@yahoo.com>
2016-03-14 18:45:42 -0400

    hey hey hey
    test

--- test/dbConnect.php
+++ test/dbConnect.php
@@ -4,6 +4,8 @@ $username = "b397f6ac809d08";
 $password = "4131b78e89f4fa9";
 $dbname = "ApartrmentRentalDB";
 
+///hey hey hey its Forrest!
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
