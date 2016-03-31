1b52bff4d66ecbfa80848c60e8f1eb5cf231b67d
Kianna Phillip <phillip_70@hotmail.com>
2016-03-31 14:17:23 -0400

    untest

--- index.php
+++ index.php
@@ -20,47 +20,6 @@
 	 
 	</div>
 	
-	</div>
-</div>
-<div class="container-fluid clearfix" id="bottom-wrapper">
-<div class="row" id="nearby-cities" style="">
-<div class="col-md-4">
-<div class="city-card" data-location-id="689996" data-track='{"name":"nearby_featured_city","view":true,"info":{"geolocated_id":689996,"geolocated_name":"Atlanta, GA","nearby_location_id":689996,"nearby_location_name":"Atlanta, GA"}}'>
-<div class="city-image">
-</div>
-<div class="city-name">
-Atlanta
-</div>
-<div class="city-text">
-Gorgeous parks, a mouth-watering food scene, and historical significance make Atlanta a wonderful city
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="city-card" data-location-id="690462" data-track='{"name":"nearby_featured_city","view":true,"info":{"geolocated_id":689996,"geolocated_name":"Atlanta, GA","nearby_location_id":690462,"nearby_location_name":"Sandy Springs, GA"}}'>
-<div class="city-image"><img class="image" src="https://res-3.cloudinary.com/apartmentlist/image/upload/s--cNiE6fDp--/c_fill,h_330,w_550/v1/web/sandy_springs-690462.jpg" />
-</div>
-<div class="city-name">
-Sandy Springs
-</div>
-<div class="city-text">
-The best of both worlds, Sandy Springs is close to the city and surrounded by nature
-</div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="city-card" data-location-id="690146" data-track='{"name":"nearby_featured_city","view":true,"info":{"geolocated_id":689996,"geolocated_name":"Atlanta, GA","nearby_location_id":690146,"nearby_location_name":"Dunwoody, GA"}}'>
-<div class="city-image"><img class="image" src="https://res-5.cloudinary.com/apartmentlist/image/upload/s--yGmQIkTK--/c_fill,h_330,w_550/v1/web/dunwoody-690146.jpg" />
-</div>
-<div class="city-name">
-Dunwoody
-</div>
-<div class="city-text">
-A city of elegance and business, Dunwoody is home to professionals and families alike
-</div>
-</div>
-</div>
-</div>
 	
 </head>
 <?php include("footer.php");?>
