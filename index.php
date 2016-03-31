5a182734e0a2a677e2abe88afe019d303c48fc1a
ayshasherin94 <ayshasherin94@yahoo.com>
2016-03-31 00:24:06 -0400

    index.php change

--- index.php
+++ index.php
@@ -7,8 +7,16 @@
 		<link rel="stylesheet" href="styles/style.css" type="text/css" />
 	
 	<div>
+	<br><br><br>
 	<h1><p id="centerHeader">Helping You Find Your New Home</p></h1>
+	<h4><p id="centerHeader"> Your personalized apartment search, from start to finish </p></h1>
+	<form>
+  <input type="text" name="search" placeholder="Search..">
+</form>
 	</div>
+	
+	
+	
 </head>
 <?php include("footer.php");?>
 <!--Simmigon -->
