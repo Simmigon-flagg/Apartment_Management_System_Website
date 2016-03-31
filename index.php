987337c9211c68affcbdd93699cda837f92f748a
ayshasherin94 <ayshasherin94@yahoo.com>
2016-03-31 17:18:14 -0400

    tag

--- index.php
+++ index.php
@@ -7,19 +7,42 @@
 		<link rel="stylesheet" href="styles/style.css" type="text/css" />
 	
 	<div>
-	<br><br><br>
+	<br>
 	<h1><p id="centerHeader">The Luxe Difference</p></h1>
 	<h4><p id="centerHeader"> Ideal Locations. Impressive Amenities. Perfect Apartments </p></h1>
 
+
 	</div>
-	
-	<div>
-	<img id="slideshow"  src="Images/Downtown 2.jpg">
-    <img id="slideshow"  src="Images/Midtown.jpg">
-	 <img id="slideshow"  src="Images/Buckhead.jpg">
-	 
-	</div>
-	
+	<br/><br/><br/>
+	 <div class="row text-center">
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src = "Images/Downtown 2.jpg" alt="Paris" width="400" height="300">
+          <p><strong>Downtown</strong></p>
+          <p>Descriptions</p>
+          <a class="btn" href="index.php"> <button class="btn">Buy Tickets</button> </a>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="Images/Midtown.jpg" alt="New York" width="400" height="300">
+          <p><strong>Midtown</strong></p>
+          <p>Descriptions</p>
+         <a class="btn" href="index.php"> <button class="btn">Buy Tickets</button> </a>
+		 <!-- <a class="navbar-brand" href="index.php">Apartment Rental</a> -->
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="Images/Buckhead.jpg" alt="San Francisco" width="400" height="300">
+          <p><strong>Buckhead</strong></p>
+          <p>Descriptions</p>
+          <a class="btn" href="index.php"> <button class="btn">Buy Tickets</button> </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 	
 </head>
 <?php include("footer.php");?>
