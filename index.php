2f628016f998cef0cccf2fe2baad69b9071c1d68
Kianna Phillip <phillip_70@hotmail.com>
2016-03-31 20:13:18 -0400

    change

--- index.php
+++ index.php
@@ -20,26 +20,23 @@
       <div class="col-sm-4">
         <div class="thumbnail">
           <img src = "Images/Downtown 2.jpg" alt="Paris" width="400" height="300">
-          <p><strong>Downtown</strong></p>
-          <p>Descriptions</p>
-          <a class="btn" href="index.php"> <button class="btn">View</button> </a>
+          <a class="btn" href="index.php"> <button class="btn">Luxe Downtown</button> </a>
+          <p><strong>Be at the center of it all</strong></p>
         </div>
       </div>
       <div class="col-sm-4">
         <div class="thumbnail">
           <img src="Images/Midtown.jpg" alt="New York" width="400" height="300">
-          <p><strong>Midtown</strong></p>
-          <p>Descriptions</p>
-         <a class="btn" href="index.php"> <button class="btn">View</button> </a>
+         <a class="btn" href="index.php"> <button class="btn">Luxe Midtown</button> </a>
+         <p><strong>Where historic significance meets a mouth- watering food scene</strong></p>
 		 <!-- <a class="navbar-brand" href="index.php">Apartment Rental</a> -->
         </div>
       </div>
       <div class="col-sm-4">
         <div class="thumbnail">
           <img src="Images/Buckhead.jpg" alt="San Francisco" width="400" height="300">
-          <p><strong>Buckhead</strong></p>
-          <p>Descriptions</p>
-          <a class="btn" href="index.php"> <button class="btn">View</button> </a>
+          <a class="btn" href="index.php"> <button class="btn">Luxe Buckhead</button> </a>
+          <p><strong>A city of elegance and business home to families and professionals alike</strong></p>
         </div>
       </div>
     </div>
