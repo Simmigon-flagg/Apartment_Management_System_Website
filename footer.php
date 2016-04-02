cee48dda5d0c2d34cf20ea85ec4bea71103cf32f
Simmigon <simmigon.flagg@gmail.com>
2016-04-01 20:16:21 -0400

    Right Panel sideNav

--- footer.php
+++ footer.php
@@ -1,17 +1,8 @@
  <hr>
 	   <h3></h3>
       <p></p>
-    <!--</div>
-	This is the right panel
-   <div class="col-sm-2 sidenav">
-	<h3>News</h3>
-      <div class="well">
-        <p>Come celebrate the graduating class of 2016!</p>
-      </div>
-      <div class="well">
-        <p>Watch out for the cold! Dress warm</p>
-      </div>
-    </div>-->
+	</div>
+<!-- Simmigon: I removes this -->
   </div>
 </div>
 
--- home.php
+++ home.php
@@ -1,3 +1,5 @@
 <?php include("header.php");?>
+<?php include("sideNav.html");?>
 <h1>Home</h1>
+<?php include("rightPanel.html");?>
 <?php include("footer.php");?>
