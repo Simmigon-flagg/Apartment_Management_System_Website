fd621334f11adba84a543362806f7ca94fef4429
ayshasherin94 <ayshasherin94@yahoo.com>
2016-04-05 15:17:55 -0400

    bill history

--- paysystem.php
+++ paysystem.php
@@ -6,11 +6,18 @@
 	<meta name="author" content="your name" />
 	<meta name="description" content="" />
 		<link rel="stylesheet" href="styles/style.css" type="text/css" />
-		
 		<br>
+		<div style="width: 600px; margin: 50px auto 0 auto;">
+<div id = "applyNow">
+		
 <h1><p id="centerHeader"><font color= "black"> View Pay System</font></p></h1>
+<h3> Your rent for this month is:<h3> <form> $
+<input type="text">
+</form>
+<br>
+<h4>Proceed to complete the payment<h4>
 
-<h2> Welcome<h2>
+
 
 <?php include("footer.php");?>
 
