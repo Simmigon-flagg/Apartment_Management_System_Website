3a2ce0c8b6156bb15cb23a8d26f477735f2091af
Simmigon <simmigon.flagg@gmail.com>
2016-04-06 15:53:47 -0400

    refactored index.phpp

--- footer.php
+++ footer.php
@@ -1,16 +1,2 @@
- <hr>
-	   <h3></h3>
-      <p></p>
-	</div>
-<!-- Simmigon: I removes this -->
-  </div>
-</div>
 
-<footer class="container-fluid text-center">
-<p>0<sup>th</sup> Team Project</p>
-  
-</footer>
-<script src="javaScriptController.js"></script>
-</body>
-</html>
 
