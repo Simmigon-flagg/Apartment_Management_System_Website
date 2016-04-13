3fc3ab9e430acdffa90b08ebc4e95e2420bb1f9e
ayshasherin94 <ayshasherin94@yahoo.com>
2016-04-13 15:02:28 -0400

    layout

--- luxeBuckhead.php
+++ luxeBuckhead.php
@@ -1,29 +1,39 @@
 <?php include("header.php"); ?>
 <html>
 <head>
-<title>My First Website</title>
-	<meta name="author" content="your name" />
-	<meta name="description" content="" />
-		<link rel="stylesheet" href="styles/style.css" type="text/css" />
-		
-		<div style="width: 700px; margin: 50px auto 0 auto;">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+</script>
+</head>
+<body>  
+<div style="width: 1000px; margin: 50px auto 0 auto;">
 	<div id = "applyNow">
 	<br>
 	<br>
-	<h1><p id="centerHeader"><font color= "black"> The Luxe Buckhead Layout of the apartment</font></p></h1>	
+	<h1><p id="centerHeader"><font color= "black"> The Luxe Midtown Layout of the apartment</font></p></h1>	
 	</div>
- <br />
- 
-<div id="container">
-    <img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300">
-	<h3> Bedroom </h3>
-    <img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300">
-    <h3> bathroom</h3>
-	<!--add on-->
-</div>
 	
-</div>
- 
-</div>
+<div data-role="main" class="ui-content">
+    <div class="ui-grid-a">
+      <div class="ui-block-a">
+        <h2><p id="centerHeader"><font color= "black">1st Bedroom</p><h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+		 <h2>2st Bedroom<h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+		 <h2>3st Bedroom<h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+		</div>
+
+      <div class="ui-block-b">
+          <h2>1st Bedroom<h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+		 <h2>1st Bedroom<h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+		 <h2>1st Bedroom<h2><br>
+        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+      </div>
+    </div>
+  </div>
+</div> 
 
 <?php include("footer.php");?>
