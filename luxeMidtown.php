824eb185b97c3a642466eae3ca98b5d30f21f65b
Kianna Phillip <phillip_70@hotmail.com>
2016-04-14 02:01:43 -0400

    pics

--- luxeMidtown.php
+++ luxeMidtown.php
@@ -9,31 +9,148 @@
 
 <div style="width: 700px; margin: 50px auto 0 auto;">
 	<div id = "applyNow">
-	<h2><p id="centerHeader"><font color= "black"><b>The Luxe Midtown Layout of the apartment</b></font></p></h1>	
-	</div>
-	</div>
-	<div style="width: 1000px; margin: 50px auto 0 auto;">
-<div data-role="main" class="ui-content">
-    <div class="ui-grid-a">
-      <div class="ui-block-a">
-      <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		 <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		 <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		</div>
-
-      <div class="ui-block-b">
-       <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		<a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		<a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
-        <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-      </div>
-    </div>
+	
+	
   </div>
 </div> 
+
+<html lang='en'>
+<head>
+<meta charset='utf-8'>
+<meta content='chrome=1; IE=9; IE=8; IE=7; IE=EDGE' http-equiv='X-UA-Compatible'>
+<meta content='width=device-width' name='viewport'>
+
+<meta content='Luxe Midtown' property='og:title'>
+<meta content='product' property='og:type'>
+
+<meta content='Images/LM.png' property='og:image'>
+
+
+</html>
+
+</a>
+</div>
+</div>
+</div>
+</header>
+
+<section id='main_content'><div class='community' id='property'>
+<div class='main_content' itemscope itemtype='http://schema.org/Residence'>
+<h1 itemprop='name'>Luxe Midtown Atlanta</h1>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge"> <!-- For intranet testing only, remove in production. -->
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <title>Slide Show</title>
+</head>
+
+<body>
+  <div id="slideShowImages">
+    <img src="Images/LM2.jpg" alt="Slide 1" />
+    <img src="Images/LM3.jpg" alt="Slide 2" />
+    <img src="Images/LM4.jpg" alt="Slide 3" />    
+    <img src="Images/LM5.jpg" alt="Slide 4" />
+  </div>  
+  <button id="slideShowButton"></button> <!-- Optional button element. -->
+  <script src="slideShow.js"></script>
+</body>
+</div>
+</div>
+
+<div class='jump' id='community_overview'>
+<h2>Community Overview</h2>
+<p itemprop='description'><div data-mercury="full" id="Property__881__long_description">Experience the excitement of Atlanta's West Midtown neighborhood. Call Luxe Midtown home and enjoy nearby King Plow Arts Center, Atlantic Station and all west side has to offer. Just moments from Downtown and Buckhead, Luxe Midtown has some of the most unique amenities in town including a saline pool with aqua lounge, outdoor theater, fitness center and billiards room.  You'll also know you're doing your part for the environment while living in responsible green luxury in one of Atlanta's first EarthCraft-certified apartment communities. With one and two bedroom apartments offering hardwood floors, 9' ceilings, granite countertops with brick backsplashes and unique built-in desks and bookcases, you'll be the envy of the neighborhood.</div></p>
+
+ </div>
+        </div>
+    </div>
+   <div id="featuresAmenities">
+        <div class="communityOverview_sectionTitle"><a id="CPH_innerContent_FeaturesAmenities" name="FeaturesAmenities" style="font-size: 22px; font-weight: bold;">Features & Amenities</a></div>
+        </div>
+        </div>
+
+                        </div>
+                        <div id="FeaturesAmenities_features_content">
+                            
+                                    <ul style="list-style-type: disc">
+                                        
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_0" class="textcontent">One-, two-, and three-bedroom apartments</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_1" class="textcontent">Designer kitchens featuring: Wood cabinetry, granite countertops, tile floors and backsplashes, stainless steel appliances, and built-in microwaves</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_2" class="textcontent">Bathrooms with wood cabinetry, granite countertops, tile floors, and soaking tubs</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_3" class="textcontent">Separate enclosed showers </span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_4" class="textcontent">Built-in desks and bookshelves </span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_5" class="textcontent">Large balconies</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_6" class="textcontent">Hardwood flooring</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_7" class="textcontent">Upgraded carpet</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_8" class="textcontent">10-foot ceilings</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_9" class="textcontent">Generous closet space</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_Features_lbl_propfeatures_10" class="textcontent">Choose from courtyard, wooded, or skyline views of Buckhead</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_3" class="textcontent">Covered, controlled-access garage parking</span></li>
+                                
+                                  
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_5" class="textcontent">Courtyard gardens with seating for relaxation and reflection</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_6" class="textcontent">Resort-style pool with sundeck </span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_7" class="textcontent">Fully equipped fitness center</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_8" class="textcontent">Clubroom with billiards table</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_9" class="textcontent">Internet cafe</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_10" class="textcontent">Grill and picnic area</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_11" class="textcontent">Storage units</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_12" class="textcontent">On-site recycling center</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_13" class="textcontent">Furnished corporate apartments</span></li>
+                                
+                                    <li style="padding-bottom: 5px;">
+                                        <span id="CPH_innerContent_lv_propertyInformation_amenities_lbl_propamen_14" class="textcontent">Fantastic Midtown location with easy access to shopping, dining, nightlife, and GA 400 </span></li>
+                                
+                                    </ul>
+                                
+                        </div>
+                    </div>
+                </div>
+
+</div>
+</div>
 
 <?php include("footer.php");?>
