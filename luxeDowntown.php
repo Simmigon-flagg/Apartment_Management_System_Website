336e9a5227104306300acc479beb0fa29de1b51f
ayshasherin94 <ayshasherin94@yahoo.com>
2016-04-13 23:49:41 -0400

    luxedowntown

--- luxeDowntown.php
+++ luxeDowntown.php
@@ -4,34 +4,50 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href="styles/style.css" type="text/css" />
 </script>
 </head>
 <body>  
 
 <div style="width: 700px; margin: 50px auto 0 auto;">
 	<div id = "applyNow">
-	<h2><p id="centerHeader"><font color= "black"><b> The Luxe Midtown Layout of the apartment</b></font></p></h2>	
+	<h2><p id="centerHeader"><font color= "black"><b><h1>The Luxe Downtown Layout of the apartment<h1></b></font></p></h2>	
 	</div>
 	</div>
+	
 	<div style="width: 1000px; margin: 50px auto 0 auto;">
 <div data-role="main" class="ui-content">
     <div class="ui-grid-a">
       <div class="ui-block-a">
-      <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+	  <figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		 <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+</figure>
+
+		<figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		 <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+</figure>
+		<figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+</figure>
 		</div>
 
       <div class="ui-block-b">
-       <a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+       <figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		<a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+</figure>
+		
+		<figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
-		<a href="#" class="ui-btn ui-corner-all ui-shadow">First bedroom</a><br>
+</figure>
+        <figure>
+	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2>Bedroom<h2></figcaption>
         <span><img src= "Images/Downtown 3.jpg" alt="Paris" width="400" height="300"></span>
+</figure>
       </div>
     </div>
   </div>
