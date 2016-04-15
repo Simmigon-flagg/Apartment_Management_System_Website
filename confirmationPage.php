43ccce6c0bbd5a25035687cec6bea56903234d74
ayshasherin94 <ayshasherin94@yahoo.com>
2016-04-14 21:33:45 -0400

    bigger font

--- confirmationPage.php
+++ confirmationPage.php
@@ -15,6 +15,20 @@
 	session_start();
 	session_destroy();
 ?>
-	<br>You are now registered and have applied for an apartment! You will receive a confirmation email after we review your application.
+
+<html>
+<head>
+	<meta name="author" content="your name" />
+	<meta name="description" content="" />
+		<link rel="stylesheet" href="styles/style.css" type="text/css" />
+
+<div style="width: 100px; margin: 40px auto 0 auto;">
+ <img src="images/checkmark.jpg" alt="check" width="150" height="150">
+</div>
+<div style="width: 1000px; margin: 40px auto 0 auto;">
+<h2>You are one step ahead! You are now registered and have applied
+ for an apartment! You will receive
+ a confirmation email after we review your application.</h2> </div>
+
 <?php include("footer.php");?>
 
