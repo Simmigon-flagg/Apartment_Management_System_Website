bbd80e2433fe6377aab0b8509a93f5cffac25b17
Kianna Phillip <phillip_70@hotmail.com>
2016-04-18 20:36:40 -0400

    shit

--- luxeMidtown.php
+++ luxeMidtown.php
@@ -12,57 +12,119 @@
 <div style="width: 700px; margin: 50px auto 0 auto;">
 	<div id = "applyNow">
 	<h2><p id="centerHeader"><font color= "black"><b><h1><font face="luxury-platinum">Luxe Midtown Atlanta<h1></b></font></p></h2>	
+	<br>
 	<h4><p id="centerHeader"><font face="ubuntu">Experience the excitement of Atlanta's West Midtown neighborhood. Call Luxe Midtown home and enjoy nearby King Plow Arts Center, Atlantic Station and all west side has to offer. Just moments from Downtown and Buckhead, Luxe Midtown has some of the most unique amenities in town including a saline pool with aqua lounge, outdoor theater, fitness center and billiards room.  You'll also know you're doing your part for the environment while living in responsible green luxury in one of Atlanta's first EarthCraft-certified apartment communities. With one and two bedroom apartments offering hardwood floors, 10' ceilings, granite countertops with brick backsplashes and unique built-in desks and bookcases, you'll be the envy of the neighborhood.</font></p></h4>
 	</div>
+</div>
+	<br>
+	<br>
+	<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<body>
+
+<div class="w3-content" style="max-width:1200px;position:relative">
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM1.jpg" style="width: 100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    One Bedroom Living & Entertainment
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM2.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    One Bedroom Master Bedroom
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM3.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    one Bedroom Kitchen
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM4.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    One Bedroom Master Bath
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM5.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Two Bedroom Guest Bedroom
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM6.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Luxe Sundeck Pool
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM7.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Two Bedroom Living & Entertainment
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM8.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Two Bedroom Kitchen
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM9.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Luxe Fitness
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="Images/LM1bed.jpg" style="width:100%">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-hor-16 w3-black">
+    Ansley Floorplan
+  </div>
+</div>
+
+<a class="w3-btn-floating w3-hover-dark-grey" style="position:absolute;top:52%;left:0" onclick="plusDivs(-1)">❮</a>
+<a class="w3-btn-floating w3-hover-dark-grey" style="position:absolute;top:52%;right:0" onclick="plusDivs(1)">❯</a>
+
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+
+</body>
+</html> 
 	</div>
 	
-	<div style="width: 1000px; margin: 50px auto 0 auto;">
-<div data-role="main" class="ui-content">
-    <div class="ui-grid-a">
-      <div class="ui-block-a">
-	  <figure>
-	  <figcaption style="width: 200px; margin: 10px auto 0 auto;"><h2><font size= "3px">Master Bedroom<h2></font></figcaption>
-        <span><img src= "Images/LM2.jpg" alt="Paris" width="450" height="350"></span>
-</figure>
-
-		<figure>
-	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2><font size= "3px">Kitchen<h2></font></figcaption>
-        <span><img src= "Images/LM3.jpg" alt="Paris" width="450" height="350"></span>
-</figure>
-		<figure>
-	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2><font size= "3px">Master Bath<h2></font></figcaption>
-        <span><img src= "Images/LM4.jpg" alt="Paris" width="450" height="350"></span>
-        
-</figure>
-		</div>
-
-      <div class="ui-block-b">
-       <figure>
-	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2><font size= "3px">Guest Bedroom<h2></font></figcaption>
-        <span><img src= "Images/LM5.jpg" alt="Paris" width="450" height="350"></span>
-</figure>
-		
-		<figure>
-	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2><font size= "3px">Sundeck Pool<h2></font></figcaption>
-        <span><img src= "Images/LM6.jpg" alt="Paris" width="450" height="350"></span>
-</figure>
-        <figure>
-	  <figcaption style="width: 200px; margin: 20px auto 0 auto;"><h2><font size= "3px">Living Area<h2></font></figcaption>
-        <span><img src= "Images/LM7.jpg" alt="Paris" width="450" height="350"></span>
-</figure>
-
-      </div>
-    </div>
-     </div>
-</li>
-</ul>
-</div>
-<div class='jump' id='specials'>
-<div class='promotion_block'>
-<p class='heading'></p>
-<p class='text'></p>
-</div>
-</div>
 <div style="width: 500px; margin: 40px auto 0 auto;">
 <h2><p id="centerHeader"><font color= "black"><b><h1><font face="luxury-platinum">Amenities & Features<h1></b></font></p></h2>	
 <ul class='tab_nav'>
@@ -70,6 +132,7 @@
       </ul>
 <div class='tab' id='ammenity_community_tab'>
 <div class='list_columns'>
+<div id="col">
 <ul>
 <li><font size="3px">One, Two, Three bedroom floorplans</li>
 <li>Two 24 Hour Fitness Centers with Free Weights</li>
@@ -87,6 +150,7 @@
     </div>
   </div>
 </div>
+</div>
 <br>
 <div style="width: 700px; margin: 50px auto 0 auto;">
 	<div id = "applyNow">
@@ -96,6 +160,6 @@
 <h2><p id="centerHeader"><font color= "black"><b><h1><font size="4px" face="luxury-platinum">Luxe Midtown<h1></b></p></h2>
 <h2><p id="centerHeader"><font color= "black"><b><h1><font size="4px" face="luxury-platinum">100 10th Street NW<h1></b></p></h2>
 <h2><p id="centerHeader"><font color= "black"><b><h1><font size="4px" face="luxury-platinum">Atlanta, GA 30309<h1></b></p></h2>
-<a href="#top">Back to top</a>
-</div>
+
+
 <?php include("footer.php");?>
